32ccb07cc961fb36b98fbb1cb3095345bfeeb850
joecqupt <joe469391363@gmail.com>
2023-03-03 00:56:37 +0800

    [wip] rx get started

---
+++ java-research/rx/src/main/java/org/example/SinkTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+import java.util.concurrent.TimeUnit;
+
+public class SinkTest {
+
+    public static void main(String[] args) throws Exception {
+        Flux<String> flux = Flux.generate(
+                () -> 1,
+                (state, sink) -> {
+                    sink.next("generate :" + state);
+                    return ++state;
+                },
+                (state) -> {
+                    System.out.println("end state:" + state);
+                }
+        );
+
+        flux.subscribe(new BaseSubscriber<String>() {
+            long subscribeTime;
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                // 这一行必须放在request前面
+                subscribeTime = System.currentTimeMillis();
+
+                subscription.request(1);
+            }
+
+            @Override
+            protected void hookOnNext(String value) {
+                System.out.println(value);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                long now = System.currentTimeMillis();
+                if ( (now - subscribeTime) > 15000) {
+                    cancel();
+                } else {
+                    request(1);
+                }
+            }
+        });
+
+
+        Thread.currentThread().join();
+    }
+}
---
+++ java-research/rx/src/main/java/org/example/SubscribeTest.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class SubscribeTest {
+    public static void main(String[] args) throws Exception {
+        Flux.interval(Duration.ofSeconds(1))
+                .subscribe(
+                        v -> System.out.println(v),
+                        t -> t.printStackTrace(),
+                        () -> System.out.println("finished")
+                );
+
+
+        Flux.range(1, 5)
+                .subscribe(new BaseSubscriber<Integer>() {
+                    @Override
+                    protected void hookOnSubscribe(Subscription subscription) {
+                        System.out.println("Subscribe");
+                        request(1);
+
+                    }
+
+                    @Override
+                    protected void hookOnNext(Integer value) {
+                        System.out.println("value:" + value);
+                        request(1);
+                    }
+                });
+
+        Thread.currentThread().join();
+    }
+}
